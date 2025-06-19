68e56577ceefcb4dcc2d67740df050df35fca15e
Keshav Gupta <keshav21@mit.edu>
2025-06-19 16:01:01 -0400

    chkpnt

--- src/top.v
+++ src/top.v
@@ -1,13 +1,13 @@
 module top(
     input wire clk100m,
-    input wire rst,
+    input wire rstn,
     output reg [15:0] leds
 );
 
     // blink the led at 1 hz
     reg [31:0] counter = 0;
     always @(posedge clk100m) begin
-        if (rst) begin
+        if (!rstn) begin
             leds <= 16'h0001;
         end else begin
             if (counter >= 50000000) begin
