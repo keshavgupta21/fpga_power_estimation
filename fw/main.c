cb2ffcf955b7d069770d6b9cba86b59d25ac10cc
Keshav Gupta <keshav21@mit.edu>
2025-06-20 11:33:40 -0400

    chkpnt

--- fw/main.c
+++ fw/main.c
@@ -8,18 +8,14 @@ XIOModule uart;
 
 void send_str(char* pstr) {
     usleep(1000);
-    while (*pstr != 0) {
-        u32 bytes_sent = XIOModule_Send(&uart, (u8*)pstr, 1);
-        pstr += bytes_sent;
+    while (*pstr != '\0') {
+        XIOModule_Send(&uart, (u8*)pstr++, 1);
         usleep(1000);
     }
 }
 
-void wait_recv_str(char* pstr, u32 n) {
-    u32 recd = 0;
-    while (recd < n) {
-        recd += XIOModule_Recv(&uart, (u8*)pstr + recd, n - recd);
-    }
+void getch(char* pstr) {
+    while (XIOModule_Recv(&uart, (u8*)pstr, 1) == 0) {}
 }
 
 int main() {
@@ -28,7 +24,7 @@ int main() {
 
     while (1) {
         send_str("Press a to print hello world, b to print goodbye\n");
-        wait_recv_str(str, 1);
+        getch(str);
         if (str[0] == 'a') {
             send_str("Hello World!\n");
         } else if (str[0] == 'b') {
