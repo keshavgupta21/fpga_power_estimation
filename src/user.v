b338be876e5647e61cf4dce9660c753c6f973ee9
Keshav Gupta <keshav21@mit.edu>
2025-06-20 11:53:26 -0400

    chkpnt

--- src/user.v
+++ src/user.v
@@ -5,11 +5,8 @@ module user(
     input wire [31:0] pwr_en_in,
     output reg [31:0] dummy_out
 );
-    // Instatiate NUM_MODULES user modules
-    localparam NUM_MODULES = 32;
-    
     genvar i;
-    generate for (i = 0; i < NUM_MODULES; i = i + 1) begin: gen_duts
+    generate for (i = 0; i < 32; i = i + 1) begin: gen_duts
         // Sandwich user module in registers
         reg pwr_en;
         wire dummy;
@@ -25,22 +22,22 @@ module user(
         end
 
 
-        // Generate random values with an LFSR (65534 cycles period)
-        reg [15:0] lfsr;
+        // // Generate random values with an LFSR (65534 cycles period)
+        // reg [15:0] lfsr;
 
-        wire bit;
-        assign bit = ((lfsr[0]) ^ (lfsr[2]) ^ (lfsr[3]) ^ (lfsr[5]));
+        // wire bit;
+        // assign bit = ((lfsr[0]) ^ (lfsr[2]) ^ (lfsr[3]) ^ (lfsr[5]));
 
-        always @(posedge clk100m) begin
-            if (!rstn) begin
-                lfsr <= (i + 1); // Initialize LFSR with module index (non-zero)
-            end else begin
-                lfsr <= {bit, lfsr[15:1]};
-            end
-        end
+        // always @(posedge clk100m) begin
+        //     if (!rstn) begin
+        //         lfsr <= (i + 1); // Initialize LFSR with module index (non-zero)
+        //     end else begin
+        //         lfsr <= {bit, lfsr[15:1]};
+        //     end
+        // end
 
         // Generate user module instances to be enabled if pwr_en is high
         //   and to generate dummy output in order to not get optimized out.
-        assign dummy = lfsr[0];
+        assign dummy = pwr_en;
     end endgenerate
 endmodule
