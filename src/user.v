97115953310ff4eac4a9eb578e8cf25d8a26b754
Keshav Gupta <keshav21@mit.edu>
2025-06-19 21:51:24 -0400

    chkpnt

--- src/user.v
+++ src/user.v
@@ -1,6 +1,22 @@
 module user(
     input wire clk100m,
-    input wire rstn
+    input wire rstn,
+
+    input wire en
 );
 
+    // Gate the clock using based on clken
+    wire clk;
+    BUFGCE clk_bufg (
+        .I(clk100m),
+        .CE(en),
+        .O(clk)
+    );
+
+    // Instatiate NUM_MODS user modules
+    localparam NUM_MODS = 100;
+    genvar i;
+    generate for (i = 0; i < NUM_MODS; i = i + 1) begin: gen_duts
+        // instantiate a single block ram
+    endgenerate
 endmodule
