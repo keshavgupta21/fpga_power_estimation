ea8a1cc2549453f2a970ecb947d121b637daf11c
Keshav Gupta <keshav21@mit.edu>
2025-06-19 23:15:00 -0400

    chkpnt

--- src/user.v
+++ src/user.v
@@ -3,9 +3,7 @@ module user(
     input wire rstn,
 
     input wire [31:0] pwr_en_in,
-    output reg [31:0] dummy_out,
-
-    output reg [15:0] leds
+    output reg [31:0] dummy_out
 );
     // Instatiate NUM_MODULES user modules
     localparam NUM_MODULES = 32;
@@ -27,7 +25,6 @@ module user(
 
         // Generate random values with an LFSR (65534 cycles period)
         reg [15:0] lfsr;
-        reg [31:0] counter;
 
         wire bit;
         assign bit = ((lfsr[0]) ^ (lfsr[2]) ^ (lfsr[3]) ^ (lfsr[5]));
@@ -35,19 +32,8 @@ module user(
         always @(posedge clk100m) begin
             if (!rstn) begin
                 lfsr <= (i + 1); // Initialize LFSR with module index (non-zero)
-                counter <= 0;
-            end else if (counter == 0) begin
-                lfsr <= {bit, lfsr[15:1]};
-            end else if (counter < 50000000) begin
-                counter <= counter + 1;
             end else begin
-                counter <= 0;
-            end
-        end
-
-        if (i == 0) begin
-            always @(posedge clk100m) begin
-                leds <= lfsr; // Assign LFSR value to LEDs for module 0
+                lfsr <= {bit, lfsr[15:1]};
             end
         end
 
